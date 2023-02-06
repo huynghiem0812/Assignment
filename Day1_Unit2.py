a1023a63e1616917f4c22db6a65b42d2cb49ab1d
huynghiem0812 <124139375+huynghiem0812@users.noreply.github.com>
2023-02-06 16:50:46 +0700

    Update Day1_Unit2.py

--- Day1_Unit2.py
+++ Day1_Unit2.py
@@ -1,5 +1,34 @@
-s = 'abracadabra'
-position = 5
-character = 'k'
-s = s[:5] + character + s[5:]
-print(s)
+def mutation(string, position, character):
+    l, i = list(), 0
+    while i < len(string):
+        if i == position:
+            l += [character]
+        else:
+            l += [string[i]]
+        i += 1
+    return ''.join(l)
+
+def get_input():
+    while True:
+        raw = input("Position: ")
+        try:
+            try:
+                num = int(raw)
+            except:
+                raise Exception("Must be a positive number! ")
+
+            if num < 0:
+                raise Exception("The number must be positive.")
+
+            return num
+
+        except Exception as e:
+            print(e)
+            continue
+
+if __name__ == '__main__':
+    s = input("String: ")
+    i = get_input()
+    c = input("Character: ")
+    s_new = mutation(s, int(i), c)
+    print(s_new)
