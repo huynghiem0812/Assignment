8d6ca7328f080bfd4c90d7cc0d52880534b4c81c
huynghiem0812 <124139375+huynghiem0812@users.noreply.github.com>
2023-02-07 09:25:00 +0700

    Update Day4_Unit2.py

--- Day4_Unit2.py
+++ Day4_Unit2.py
@@ -1,7 +1,11 @@
 import requests
 from lxml import html
+from datetime import datetime
 
-url = 'https://www.us-cert.gov/ncas/alerts'
-doc = html.fromstring(requests.get(url).text)
-print("The number of security alerts issued by US-CERT in the current year:")
-print(len(doc.cssselect('.item-list li')))
+current_year = datetime.now().year - 1
+url = 'https://www.us-cert.gov/ncas/alerts/' + str(current_year)
+for page in range(0, 2):
+    req = requests.get(url + '?page=' + str(page))
+    doc = html.fromstring(req.text)
+    print("The number of security alerts issued by US-CERT in " + str(current_year)+" page " + str(page) + ":")
+    print(len(doc.cssselect('.item-list li')))
